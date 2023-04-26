1bf0d2373e607b46cfd18bf6f7b5c6afd099e1ba
Simon Stibbards <simon.stibbards@bagelcode.com>
2023-04-26 11:15:11 +0100

    Initial commit

---
+++ Minesweeper_Unity/Assets/Scripts/Model/MineTile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Minesweeper
+{
+    /// <summary>
+    /// A class to hold the state of a Minesweeper tile
+    /// </summary>
+    public class MineTile : MonoBehaviour
+    {
+        public Text text;
+        private Image image;
+        private Image flag;
+        public ButtonLongPress button;
+        public Animator anim;
+
+        public bool isRevealed;
+        public bool isBomb;
+        public bool isFlagged;
+        public int x, y;
+    }
+}
